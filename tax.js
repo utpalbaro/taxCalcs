84901dc7c067105fd1382fabd6eb42f3bbaf7767
Utpal Baro <utpalbaro@gmail.com>
2020-07-06 22:50:01 +0530

    Added tax-slabs-india.webp file Added ability to take a tax slab table to calculate tax

--- tax.js
+++ tax.js
@@ -1,30 +1,30 @@
-function calculateTaxWithGivenRates(taxableIncome, slabs, rates) {
-    let lowerLimit = slabs[slabs.length - 1];
-    let taxRate = rates[rates.length - 1];
+function calculateTaxWithGivenRates(taxableIncome, slabs) {
+    const rows = slabs.length;
+    const cols = 2;
+
+    let lowerLimit = slabs[rows - 1][0];
+    let taxRate = slabs[rows - 1][1];
     let tax = 0;
 
     for (let i = 1; i < slabs.length; ++i) {
-        if (taxableIncome < slabs[i]) {
-            lowerLimit = slabs[i-1];    // the lower limit
-            taxRate = rates[i-1];       // the applicable tax rate
+        if (taxableIncome < slabs[i][0]) {
+            lowerLimit = slabs[i-1][0];    // the lower limit
+            taxRate = slabs[i-1][1];       // the applicable tax rate
 
             tax = tax + taxRate*(taxableIncome - lowerLimit);
             break;
         }
         else
-            tax = tax + rates[i-1]*(slabs[i] - slabs[i-1]);
+            tax = tax + slabs[i-1][1]*(slabs[i][0] - slabs[i-1][0]);
     }
 
-    tax = tax + rates[rates.length-1]*(Math.max(0, taxableIncome - slabs[slabs.length-1]));
+    tax = tax + slabs[rows-1][1]*(Math.max(0, taxableIncome - slabs[rows-1][0]));
 
     return tax;
 }
 
-function calculateTax(taxableIncome) {
-    let slabs = [0, 250000, 500000, 1000000];
-    let rates = [0, 0.05, 0.2, 0.3];
-
+function calculateTax(taxableIncome, slabs) {
     return  Array.isArray(taxableIncome) ? 
-            taxableIncome.map(row => row.map(cell => calculateTaxWithGivenRates(cell, slabs, rates))) :
-            calculateTaxWithGivenRates(taxableIncome, slabs, rates);
+            taxableIncome.map(row => row.map(cell => calculateTaxWithGivenRates(cell, slabs))) :
+            calculateTaxWithGivenRates(taxableIncome, slabs);
 }
